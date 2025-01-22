5fe75c76a0f353d213cfc3626d6d6cee851bb249
Edgar Alessandro Balbuena Sandoval <141881064+EdgarBalbuena@users.noreply.github.com>
2025-01-21 19:47:57 -0600

    Crea app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaDeAmigos = [];
+let validar = true;
+function agregarAmigo()
+{
+    listaDeAmigos.push((document.getElementById('amigo')).value); //.value toma el valor de la caja
+    console.log(listaDeAmigos)
+}
+
+
+
