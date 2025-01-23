bb3989c319c2c0bf477a075c0234701bb5ef7fa4
Edgar Alessandro Balbuena Sandoval <141881064+EdgarBalbuena@users.noreply.github.com>
2025-01-23 00:43:48 -0600

    código completado

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
-// Aquí deberás desarrollar la lógica para resolver el problema.
 
 let listaDeAmigos = [];
-let validar = true;
-function agregarAmigo()
-{
-    listaDeAmigos.push((document.getElementById('amigo')).value); //.value toma el valor de la caja
-    console.log(listaDeAmigos)
+
+const listaAmigos = document.getElementById('listaAmigos');
+const amigoSorteado = document.getElementById('resultado');
+
+function agregarAmigo(){
+    const amigo = document.getElementById('amigo').value; //.value toma el valor de la caja 
+    if (amigo !== ''){ //!Verificar que no este vacio el cuadro de texto
+        listaDeAmigos.push(amigo);
+        console.log(listaDeAmigos)
+    }
+    else{
+        alert('Por favor, inserte un nombre')
+    }
+    listaAmigos.innerHTML += "<li>" + amigo + "</li>";
+    limpiarCaja();
 }
 
+function sortearAmigo(){
+    if (listaDeAmigos.length > 0){
+        let indiceRandom = Math.floor(Math.random()*(listaDeAmigos.length));
+        amigoSorteado.innerHTML = '<li>' + listaDeAmigos[indiceRandom] + '</li>'
+        listaAmigos.innerHTML = ''  //Limpia los nombres previamente ingresados para mostrar solo el nombre sorteado
+    }
+    else{
+        alert('No hay amigos para sortear')
+    }    
+}
 
+function limpiarCaja(){
+    let vaciarCaja = document.getElementById('amigo');
+    vaciarCaja.value = '';
+}
 
